1ac83805852b402ef4eb06f1371150204e3067bf
Justin Largo <justinlargo7@gmail.com>
2026-01-24 20:25:05 -0500

    added my own zen zsh shell

---
+++ dot_config/nvim/lua/plugins/private_formatting.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sqlfluff" },
+      },
+      formatters = {
+        sqlfluff = {
+          command = "sqlfluff",
+          args = { "fix", "--dialect", "postgres", "$FILENAME" },
+          stdin = false,
+        },
+      },
+    },
+  },
+}
